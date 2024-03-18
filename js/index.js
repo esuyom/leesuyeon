b5b69547f2dba31c4edf4a6898ab0be19bd9f35e
esuyom <sylee0560@gmail.com>
2024-03-18 14:59:05 +0900

    포폴 수정

---
+++ js/index.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+    // 마우스 커서
+    $(window).mousemove(function (e) {
+        $(".ring").css(
+            "transform",
+            `translateX(calc(${e.clientX}px - 1.25rem)) translateY(calc(${e.clientY}px - 1.25rem))`
+        );
+    });
+    $("#wrap.main #main .wall1 .txt2 .txt").hover(function(){
+        $("#cursor").addClass("type1");
+    }, function(){
+        $("#cursor").removeClass("type1");
+    });	
+
+    $("#wrap.main #main .wall2 .txt2 .txt").hover(function(){
+        $("#cursor").addClass("type2");
+    }, function(){
+        $("#cursor").removeClass("type2");
+    });	
+
+    // profile
+    $("#wrap.main #main .wall1 .profile").click(function(){
+        $("#profile").addClass("active");
+    });
+
+    // work
+    $("#wrap.main #main .wall2 .work").click(function(){
+        $("#work").addClass("active");
+    });
+
+    $("section.side .close").click(function(){
+        $("section.side").removeClass("active");
+    });
+});
